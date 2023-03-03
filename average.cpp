7b80a8555eaf84d6c6158f0fc9534849a20db179
P <tatakornaow@gmail.com>
2023-03-03 15:18:31 +0700

    aver done

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+using namespace std;
 
+int main(int argc,char* atgv[])
+{
+    if(argc == 1) cout << "Please input numbers to find average.";
+    else{
+        double plus=0,average;
+        for (int i=1;i<argc;i++)
+        {
+            plus += atof(atgv[i]);
+        }
+        average = plus / (argc-1);
+        cout << "---------------------------------\n";
+        cout << "Average of "<<argc-1<<" numbers = "<<average<<endl;
+        cout << "---------------------------------";
+        return 0;
+    }
+}
